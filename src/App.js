1e9bd93711d9f64cecfff57a888114a6caed539c
Sudipta <sudiptarumu@gmail.com>
2019-12-09 12:29:04 +0600

    routing for hats page drafted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+
+const Hats=()=>(
+  <div>hats page</div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component= {Homepage}/>
+        <Route path='/hats' component= {Hats}/>
+      </Switch>
+      
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './homepage.style.scss'
+import Directory from '../../component/directory/directory.component';
+
+
+const Homepage =() => (
+
+    <div className='homepage'>
+          <Directory />
+    </div>
+
+) 
+export default Homepage;
